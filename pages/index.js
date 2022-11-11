dc027650d65886d162486ee742bb3b7a3e52437b
JulianWagner <juliantosun@gmail.com>
2022-11-11 12:51:51 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -189,11 +189,14 @@ export default function Page(props) {
                 One of the most trusted photographers in entertainment.<br/><br/> Greg has always loved shooting on his phone but missed the ergonomic grip and shutter of a camera – hence designing the&nbsp;G-Grip.
               </p>
             </div>
+            <div>
             <p className="mt-4 md:mt-0">
               Greg says... <br/><br/>
               <span className="italic"> I shoot pictures on my phone everyday. I invented the G-Grip so I can operate
                                         it like a traditional&nbsp;camera.</span>
             </p>
+              <p className="mt-4">Follow Greg on <a className="text-neutral-400" href="https://www.instagram.com/gregwilliamsphotography/">Instagram</a></p>
+            </div>
           </div>
           <h2 className="mt-16 max-w-sm mx-auto text-center">
             Battery life 20 hours in use <br/> and 4,000 hours on standby. </h2>
