06dd1cdf17baedbbf6dd033c837223e750772cdc
JulianWagner <juliantosun@gmail.com>
2022-11-12 16:00:18 +0100

    update

--- pages/api/getCountry.js
+++ pages/api/getCountry.js
@@ -1,12 +1,10 @@
 import axios from 'axios'
+import requestIp from 'request-ip'
 
 
 export default async function handler(req, res) {
-  // read the location from the request header
-  const ip = req.socket.remoteAddress
-  console.log(ip)
+  const ip = requestIp.getClientIp(req);
   const {data} = await axios.get(`http://ip-api.com/json/${ip}?fields=status,message,continent,country,query`)
-  console.log(data)
 
     res.send({
       data
