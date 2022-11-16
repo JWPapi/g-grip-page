ced3f7b02a0a6d4f4fd12a4930072c2341be9f69
JulianWagner <juliantosun@gmail.com>
2022-11-16 16:52:23 +0100

    update

--- components/NavBar.js
+++ components/NavBar.js
@@ -20,7 +20,7 @@ export default function NavBar({checkoutURL}) {
         <Link href="/#about"><a className="hover:text-gray-900 mx-2">About</a></Link>
         <Link href="/#techspecs"><a className="hover:text-gray-900 mx-2 text-body font-body">Tech Specs</a></Link>
         <a href={checkoutURL}>
-          <button className="px-4 py-2 mx-2">Pre-order now</button>
+          <button className="ml-6 px-8 py-2">Pre-order now</button>
         </a>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,39 @@
 import Head from "next/head"
 import Image from "next/image"
 import Footer from "../components/Footer"
-import swell from "swell-js"
 import NavBar from "../components/NavBar";
-swell.init('g-grip','pk_4yeESF2sYLvnuIY42XlQ6vPZJTSv6Mro')
 
 // ToDo: Separate in smaller components
+// ToDo: Consistent margin cascading
 export default function Page({checkoutURL, price}) {
 
   return (<>
     <Head>
       <title>G–Grip by Greg Williams</title>
     </Head>
-    <NavBar checkoutURL={checkoutURL} />
-    <div className="absolute w-screen anchor" id="top" >
+    <NavBar checkoutURL={checkoutURL}/>
+    <div className="absolute w-screen anchor" id="top">
       <main>
         <div>
           <div className="flex flex-col justify-center px-4 pt-8">
             <Image src="/img/logo_big_red.svg"
-              alt="G-Grip Logo"
-              width={567}
-              height={209}/>
+                   alt="G-Grip Logo"
+                   width={567}
+                   height={209}/>
           </div>
           <div className="my-8">
             <div className="mb-8 md:mb-16 mt-8">
               <video autoPlay
-                muted
-                playsInline
-                loop>
+                     muted
+                     playsInline
+                     loop>
                 <source src="/videos/introVideo.mp4"
-                  type="video/mp4"/>
+                        type="video/mp4"/>
               </video>
             </div>
             <div className="px-4 md:px-0">
               <h2 id="overview"
-                className="px-0 anchor">Hold your phone like a camera</h2>
+                  className="px-0 anchor">Hold your phone like a camera</h2>
               <div className="c-grid-2 lg:px-0">
                 <div>
                   <h5>Universally compatible</h5>
@@ -47,7 +46,8 @@ export default function Page({checkoutURL, price}) {
                 </div>
                 <div className="mt-4 md:mt-0">
                   <p>
-                    A Bluetooth® shutter button in an ergonomically designed grip that enables you to hold and shoot one-handed like a traditional&nbsp;camera.
+                    A Bluetooth® shutter button in an ergonomically designed grip that enables you to hold and shoot
+                    one-handed like a traditional&nbsp;camera.
                   </p>
                 </div>
               </div>
@@ -57,17 +57,27 @@ export default function Page({checkoutURL, price}) {
         </div>
         <section className="mb-8 md:mb-16 anchor">
           <Image src={"/img/g-grip-hand-on-iphone.jpg?v=2"}
-            alt="ggrip_holding"
-            width={2400}
-            height={1600}/>
+                 alt="ggrip_holding"
+                 width={2400}
+                 height={1600}/>
           <div className="grid grid-cols-2 gap-2 md:gap-4 mt-1 md:mt-2">
             <Image src={"/img/g-grip-on-iphone.jpg"}
-              alt="ggrip_on_iphone"
-              width={2400}
-              height={1600}/> <Image src={"/img/g-grip-fitting-to-iphone.jpg"}
-            alt="ggrip_attaching"
-            width={2400}
-            height={1600}/>
+                   alt="ggrip_on_iphone"
+                   width={2400}
+                   height={1600}/>
+            <Image src={"/img/g-grip-fitting-to-iphone.jpg"}
+                   alt="ggrip_attaching"
+                   width={2400}
+                   height={1600}/>
+            <Image src={"/img/g-grip-on-iphone-portrait.jpg"}
+                   alt="ggrip_on_iphone_portrait"
+                   width={2400}
+                   height={1600}/>
+            <Image src={"/img/g-grip-on-iphone-portrait-2.jpg"}
+                   alt="ggrip_on_iphone_portrait_2"
+                   width={2400}
+                   height={1600}/>
+
           </div>
         </section>
 
@@ -75,11 +85,11 @@ export default function Page({checkoutURL, price}) {
           <h2>The G-Grip in action</h2>
           <div className="mt-4 mb-8">
             <video autoPlay
-              muted
-              playsInline
-              loop>
+                   muted
+                   playsInline
+                   loop>
               <source src="/videos/g_grip_in_action.mp4"
-                type="video/mp4"/>
+                      type="video/mp4"/>
             </video>
           </div>
         </section>
@@ -88,11 +98,11 @@ export default function Page({checkoutURL, price}) {
           <div className="text-9xl leading-none text-center mb-4 -mt-4 tracking-tighter">{price}</div>
           <div className="text-center mb-32 ">
             <a href={checkoutURL}>
-              <button className="text-2xl px-8 py-4">
+              <button className="text-2xl px-10 py-3">
                 Pre-order now
               </button>
             </a>
-            <div className="mt-4">Free worldwide shipping • 30 day no risk return </div>
+            <div className="mt-4">Free worldwide shipping • 30 day no risk return</div>
           </div>
         </section>
         <section className="mb-4 md:mb-12 anchor" id="about">
@@ -100,20 +110,20 @@ export default function Page({checkoutURL, price}) {
           <div className="mt-4 mb-8">
             <div className="md:hidden">
               <iframe src="https://player.vimeo.com/video/769619524?h=fc1cc5987c"
-                width="1024"
-                height="576"
-                style={{maxWidth: "100%",height: 'calc(100vw * 0.5625)'}}
-                frameBorder="0"
-                allow="autoplay; fullscreen; picture-in-picture"
-                allowFullScreen></iframe>
+                      width="1024"
+                      height="576"
+                      style={{maxWidth: "100%", height: 'calc(100vw * 0.5625)'}}
+                      frameBorder="0"
+                      allow="autoplay; fullscreen; picture-in-picture"
+                      allowFullScreen></iframe>
             </div>
             <div className="hidden md:block">
               <iframe src="https://player.vimeo.com/video/769619524?h=fc1cc5987c"
-                width="1024"
-                height="576"
-                frameBorder="0"
-                allow="autoplay; fullscreen; picture-in-picture"
-                allowFullScreen></iframe>
+                      width="1024"
+                      height="576"
+                      frameBorder="0"
+                      allow="autoplay; fullscreen; picture-in-picture"
+                      allowFullScreen></iframe>
             </div>
           </div>
         </section>
@@ -122,7 +132,8 @@ export default function Page({checkoutURL, price}) {
             <div>
               <h5>Greg Williams</h5>
               <p>
-                One of the most trusted photographers in entertainment.<br/><br/> Greg has always loved shooting on his phone but missed the ergonomic grip and shutter of a camera – hence designing the&nbsp;G-Grip.
+                One of the most trusted photographers in entertainment.<br/><br/> Greg has always loved shooting on his
+                phone but missed the ergonomic grip and shutter of a camera – hence designing the&nbsp;G-Grip.
               </p>
             </div>
             <div>
@@ -132,18 +143,16 @@ export default function Page({checkoutURL, price}) {
                                         it like a traditional&nbsp;camera.</span>
               </p>
               <p className="mt-4">Follow Greg on <a className="text-neutral-400"
-                href="https://www.instagram.com/gregwilliamsphotography/">Instagram</a></p>
+                                                    href="https://www.instagram.com/gregwilliamsphotography/">Instagram</a>
+              </p>
             </div>
           </div>
-
         </section>
-        <section>
-          <h2 className="mt-16 max-w-sm mx-auto text-center">
-            Battery life 20 hours in use <br/> and 4,000 hours on standby. </h2>
-            <div className="text-uxl text-center -mt-6 tracking-tighter line-height leading-none">20</div>
-            <div className="text-6xl text-center -mt-4 mb-16">hours</div>
-          <h2 id="techspecs"
-            className="anchor">Technical specifications</h2>
+        <section className="mt-16">
+          <div className="text-uxl text-center -mt-6 tracking-tighter line-height leading-none">20</div>
+          <h2 className=" max-w-sm mx-auto text-center">
+            hours battery life in normal use <br/> and 4,000 hours on standby. </h2>
+          <h2 id="techspecs" className="anchor mt-16 md:mt-32">Technical specifications</h2>
           <div className="c-grid-2 mb-16 px-4 md:p-0">
             <div>
               <h5>Usage</h5>
@@ -167,9 +176,14 @@ export default function Page({checkoutURL, price}) {
           <div className="c-grid-2 px-4 xl:px-0">
             <div>
               <h5>Attaching the G-Grip plate to your phone</h5>
-              <p>G-Grip comes with 2 x 3M VHB single use adhesive backed plates which will adhere to smooth, non-textured, non-porous, flat surfaces. We recommend a hard plastic or leather case.* Position the plate using the guide supplied, leave for 24hrs to harden fully before attaching the G-Grip.</p>
+              <p>G-Grip comes with 2 x 3M VHB single use adhesive backed plates which will adhere to smooth,
+                non-textured, non-porous, flat surfaces. We recommend a hard plastic or leather case.* Position the
+                plate using the guide supplied, leave for 24hrs to harden fully before attaching the G-Grip. Choose
+                between landscape ot portrait position.
+              </p>
 
-              <small className="mt-2 block"> * They will not adhere to rubber, matte glass, silicon, TPU or soft touch&nbsp;coatings.</small>
+              <small className="mt-2 block"> * They will not adhere to rubber, matte glass, silicon, TPU or soft
+                touch&nbsp;coatings.</small>
               <h5 className="mt-4">Turning on</h5>
               <ul>
                 <li>Press and hold for 3 seconds.</li>
@@ -192,7 +206,8 @@ export default function Page({checkoutURL, price}) {
                 <li>For Burst mode, press and hold (for up to 3 minutes).</li>
               </ul>
               <p className="mt-4">
-                Note: To configure Burst mode on iPhone, go to Settings &gt; Camera and enable the option to ‘Use Volume Up for Burst’.
+                Note: To configure Burst mode on iPhone, go to Settings &gt; Camera and enable the option to ‘Use Volume
+                Up for Burst’.
               </p>
               <h5 className="mt-4">Shooting videos</h5>
               <ul>
@@ -211,29 +226,43 @@ export default function Page({checkoutURL, price}) {
           <div className="text-9xl leading-none text-center mb-4 -mt-4 tracking-tighter">{price}</div>
           <div className="text-center mb-32 ">
             <a href={checkoutURL}>
-              <button className="text-2xl px-8 py-4">
+              <button className="text-2xl px-10 py-3">
                 Pre-order now
               </button>
             </a>
-            <div className="mt-4">Free worldwide shipping • 30 day no risk return </div>
+            <div className="mt-4">Free worldwide shipping • 30 day no risk return</div>
           </div>
         </section>
         <section>
           <hr className="mb-2"/>
           <div className="text-neutral-500 mb-8 text-xs md:text-sm px-4 xl:px-0 flex flex-col gap-4">
-            <p>The user is cautioned that changes or modifications not expressly approved by the party responsible for compliance could void the user’s authority to operate the equipment. This device complies with Part 15 of the FCC Rules. Operation is subject to the following two conditions: (1) this device may not cause harmful interference, and (2) this device must accept any interference received, including interference that may cause undesired operation. NOTE: This equipment has been tested and found to comply with the limits for a Class B digital device, pursuant to Part 15 of the FCC Rules. These limits are designed to provide reasonable protection against harmful interference in a residential installation. This equipment generates, uses and can radiate radio frequency energy and, if not installed and used in accordance with the instructions, may cause harmful interference to radio communications. However, there is no guarantee that interference will not occur in a particular installation. If this equipment does cause harmful interference to radio or television reception, which can be determined by turning the equipment off and on, the user is encouraged to try to correct the interference by one or more of the following measures:
+            <p>The user is cautioned that changes or modifications not expressly approved by the party responsible for
+              compliance could void the user’s authority to operate the equipment. This device complies with Part 15 of
+              the FCC Rules. Operation is subject to the following two conditions: (1) this device may not cause harmful
+              interference, and (2) this device must accept any interference received, including interference that may
+              cause undesired operation. NOTE: This equipment has been tested and found to comply with the limits for a
+              Class B digital device, pursuant to Part 15 of the FCC Rules. These limits are designed to provide
+              reasonable protection against harmful interference in a residential installation. This equipment
+              generates, uses and can radiate radio frequency energy and, if not installed and used in accordance with
+              the instructions, may cause harmful interference to radio communications. However, there is no guarantee
+              that interference will not occur in a particular installation. If this equipment does cause harmful
+              interference to radio or television reception, which can be determined by turning the equipment off and
+              on, the user is encouraged to try to correct the interference by one or more of the following measures:
             </p>
             <ul className="list-decimal ml-4">
               <li>Reorient or relocate the receiving antenna</li>
               <li>Increase the separation between the equipment and receiver</li>
-              <li>Connect the equipment into an outlet on a circuit different from that to which the receiver is connected
+              <li>Connect the equipment into an outlet on a circuit different from that to which the receiver is
+                connected
               </li>
               <li>Consult the dealer or an experienced radio/TV technician for help</li>
             </ul>
             <div>
               <h5>FCC Radiation Exposure Statement</h5>
               <p>
-                This equipment complies with FCC radiation exposure limits set forth for an uncontrolled environment. This transmitter must not be co-located or operating in conjunction with any other antenna or transmitter.
+                This equipment complies with FCC radiation exposure limits set forth for an uncontrolled environment.
+                This transmitter must not be co-located or operating in conjunction with any other antenna or
+                transmitter.
               </p>
             </div>
             <div>
