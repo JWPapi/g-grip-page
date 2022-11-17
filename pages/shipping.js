e3918c57c6e4dac96657308ce57ba893584f801e
JulianWagner <juliantosun@gmail.com>
2022-11-17 15:04:24 +0100

    update

--- pages/shipping.js
+++ pages/shipping.js
@@ -1,105 +1,116 @@
 import Footer from "@/../../components/Footer";
+import Head from "next/head";
 
-export default function Shipping () {
-  return (
-    <div className="pt-16">
-      <article className="mx-auto prose my-16">
-        <h1>Shipping & Delivery</h1>
-        <p>
-          We advise on a delivery period of up to two (2) to three (3) weeks for your print(s) or other products e.g
-          cameras or camera equipment. If you would like your print(s) or other purchases e.g. cameras or camera
-          equipment sooner, please contact us at studio@gwp.wordpress.test for stock information. International orders
-          may take longer especially to the EU due to the impact of Brexit.
-        </p>
-        <p> All prints, cameras and camera equipment are listed at retail value. Shipping is free on orders over five
+export default function Shipping() {
+  return (<>
+      <Head>
+        <title>G–Grip by Greg Williams – Shipping & Refund Policy</title>
+      </Head>
+      <div className="pt-16 px-4 md:px-0">
+        <article className="mx-auto prose my-16">
+          <h1>Shipping & Delivery</h1>
+          <p>
+            We advise on a delivery period of up to two (2) to three (3) weeks for your print(s) or other products e.g
+            cameras or camera equipment. If you would like your print(s) or other purchases e.g. cameras or camera
+            equipment sooner, please contact us at studio@gwp.wordpress.test for stock information. International orders
+            may take longer especially to the EU due to the impact of Brexit.
+          </p>
+          <p> All prints, cameras and camera equipment are listed at retail value. Shipping is free on orders over five
             hundred pounds sterling (£500), otherwise we will notify you of domestic UK shipping costs. We may request
             that you cover any re-delivery costs where we have not been able to deliver because of incorrect or
             incomplete information provided to us. Overseas shipping costs and customs duties on imports will be
             additional and will be at your cost.
-        </p>
-        <p> Last updated: February 2021
-        </p>
-        <h1>Return Policy</h1>
-        <p>
-          Our policy is applicable for fourteen (14) days after you have received your order. If fourteen (14) days have
-          gone by since receipt, unfortunately we can’t offer you refunds or exchanges. This is in addition to your
-          statutory rights.
-        </p>
-        <p> i) Refunds
+          </p>
+          <p> Last updated: February 2021
+          </p>
+          <h1>Return Policy</h1>
+          <p>
+            Our policy is applicable for fourteen (14) days after you have received your order. If fourteen (14) days
+            have
+            gone by since receipt, unfortunately we can’t offer you refunds or exchanges. This is in addition to your
+            statutory rights.
+          </p>
+          <p> i) Refunds
             If you are unhappy with your purchase you may return it in its original condition for a refund, provided it
             is returned within fourteen (14) days from receipt by you of your order. Shipping costs will not be
             refunded. Please contact studio@gwp.wordpress.test for further instructions.
-        </p>
-        <p> To be eligible for a refund, your item must be unused, undamaged and in the same condition that you received
+          </p>
+          <p> To be eligible for a refund, your item must be unused, undamaged and in the same condition that you
+            received
             it. It must also be in the original packaging.
-        </p>
-        <p> Once your return is received and inspected, we will send you an email to notify you that we have received
+          </p>
+          <p> Once your return is received and inspected, we will send you an email to notify you that we have received
             your returned item. Provided the item is unused, undamaged and in the same condition as you received it, we
             will grant a refund. This will occur via a credit being automatically applied to your credit card or
             original method of payment.
-        </p>
-        <p> ii) Exchanges
+          </p>
+          <p> ii) Exchanges
             Exact exchanges will be made on damaged or defective products. If a product arrives damaged, you must keep
             all original packaging material for insurance purposes. Claims must be made within fourteen (14) days of
             receipt of your order via email to studio@gwp.wordpress.test.
-        </p>
-        <p> In the event of an exchange or refund, please print off your order confirmation form which includes your
+          </p>
+          <p> In the event of an exchange or refund, please print off your order confirmation form which includes your
             unique order Number and return it with the damaged or defective item to the following address:
-        </p>
-        <p> a. For Prints:
+          </p>
+          <p> a. For Prints:
             Greg Williams Photography c/o Dark Matter, 8 Brook Lane Business Centre, Brentford, Middlesex TW8 0PP.
-        </p>
-        <p> b. For Camera Equipment:
+          </p>
+          <p> b. For Camera Equipment:
             Greg Williams Photography, Lower Ground Floor, 34 North Row, London W1K 6DG United Kingdom.
-        </p>
-        <p> In addition to insuring your return, we strongly recommend that the order be sent with Delivery
+          </p>
+          <p> In addition to insuring your return, we strongly recommend that the order be sent with Delivery
             Confirmation. Should we not receive your return, we will not be able to issue a refund or exchange as
             appropriate. Refunds on returns will be issued on the price of the merchandise and any sales tax, if
             applicable. If the purchase is made in a currency other than GBP £, a refund will be based on the daily rate
             of the date of purchase.
-        </p>
-        <p> You will be responsible for paying for your own shipping costs for returning your item. Shipping costs are
+          </p>
+          <p> You will be responsible for paying for your own shipping costs for returning your item. Shipping costs are
             non-refundable. We will not charge you for any further shipping costs on exchanges where we send replacement
             goods.
-        </p>
-        <p> Items that are returned to us as undeliverable or returned after three (3) delivery attempts will be treated
+          </p>
+          <p> Items that are returned to us as undeliverable or returned after three (3) delivery attempts will be
+            treated
             as a return. Shipping will be charged again upon the request of an order reshipment. We will try and contact
             you three (3) times through the email address provided with your order. If two (2) months have passed, and
             we have not received a response from you, your item will be returned-to-stock, and you will be refunded for
             the product cost, but not the shipping costs, associated with this order.
-        </p>
-        <p> Late or Missing Refunds (if applicable)
+          </p>
+          <p> Late or Missing Refunds (if applicable)
             If you haven’t received a refund yet, first double check your account. Then contact your credit card
             company, as it may take some time before your refund is processed.
-        </p>
-        <p> If you’ve done all of this and you still have not received your refund, please contact us at info@gwshop.uk.
+          </p>
+          <p> If you’ve done all of this and you still have not received your refund, please contact us at
+            info@gwshop.uk.
             Depending on where you live, the time it may take for your exchanged product to reach you may vary.
-        </p>
-        <p> Delivery Gone Missing
+          </p>
+          <p> Delivery Gone Missing
             If your order seems to be lost in the mail, please notify us by email at info@gwshop.uk no later than three
             (3) weeks for domestic orders, and five (5) weeks for international orders, after having received your
             shipping notification. We will do our best to work with you and the shipping service to locate your package,
             and/or send you a replacement. Unfortunately, we are unable to assist after this time frame has passed.
-        </p>
-        <p> Warranties
+          </p>
+          <p> Warranties
             Leica Camera AG (“LEICA”) offers various product warranties from the date of purchase from an authorised
             LEICA dealership (which includes GWP) for any LEICA product and all accessories included in the original
             packaging. The product warranty is subject to various warranty conditions. For further information please
             see here: https://uk.leica-camera.com/warranty.
-        </p>
-        <p> Your statutory warranty rights towards the seller of your LEICA products remain unaffected by such product
+          </p>
+          <p> Your statutory warranty rights towards the seller of your LEICA products remain unaffected by such product
             warranties.
-        </p>
-        <p> Last updated: February 2021
-        </p>
+          </p>
+          <p> Last updated: February 2021
+          </p>
 
-      </article>
-      <Footer />
-    </div>
+        </article>
+      </div>
+      <Footer/>
+
+    </>
   )
 
 }
-export async function getStaticProps () {
+
+export async function getStaticProps() {
   return {
     props: {
       title: 'Shipping & Delivery',
--- pages/terms.js
+++ pages/terms.js
@@ -1,45 +1,103 @@
 import Footer from "../components/Footer"
+import Head from "next/head";
 
 export default function Terms() {
-  return (
-    <div className="pt-16">
+  return (<>
+      <Head>
+        <title>G–Grip by Greg Williams</title>
+      </Head>
 
-      <article className="mx-auto prose my-16">
-        <h1>Terms & Conditions</h1>
-        <p>
-          The pages on the website (“the Website”) are published by Greg Williams Photography Limited, registered in England at PO Box 1295, 20 Station Road, Gerrards Cross, Buckinghamshire SL9 8EL with company registration number 04557292 (“GWP”).
-        </p>
-        <p> The Website is intended to serve as an introduction to our company, by giving an outline of the products and services provided through the Website and by providing details of how we can be contacted. The terms and conditions set out below govern the use by you of the Website. We reserve the right to change the terms of use of the Website at any time and you are requested to check the terms of use from time to time to ensure that you have seen the latest version.
-        </p>
-        <p> Copyright and Trademarks Subject to the provisions below, the copyright in all content, graphics, tables, images on the Website belongs to us or our clients and/or licensors and is hereby asserted. Any copying or modification of all or any part of the Website is hereby prohibited with the exception that clients and prospective clients are welcome to take a copy of the pages of the Website, which do not contain materials relating to our clients, as a record of its contents and for personal use only.
-        </p>
-        <p> The Website also contains materials belonging to our clients. These are displayed on the Website with their kind permission, for the purpose of promoting the range of services offered by us and are for display only. Any copying of such materials is strictly prohibited. Please contact us if you require any further clarification in this regard. We hereby assert and reserve all our copyright, design right, moral rights, the moral rights of our employees, community design rights and all other rights in law of whatever type anywhere in the world to protect our intellectual property, goodwill and reputation.
-        </p>
-        <p> If you purchase a print from the Website, please be aware you are purchasing that print only and not any underlying copyright in such print. Please ensure that the prints are not reproduced or published in any other manner or media.
-        </p>
-        <p> Online Commerce You agree to be financially responsible for all purchases made by you or someone acting on your behalf through the Website. You agree to use the Website and to purchase services or products through the Website for legitimate, non-commercial purposes only. You also agree not to make any purchases for speculative, false or fraudulent purposes or for the purpose of anticipating demand for a particular product or service. You agree to only purchase goods or services for yourself or for another person for whom you are legally permitted to do so. When making a purchase for a third party that requires you to submit the third party’s Personally Identifiable Information to us or a merchant, you represent that you have obtained the express consent of such third party to provide such third party’s information.
-        </p>
-        <p> Disclaimers Throughout the Website, we may provide links and pointers to other sites maintained by third parties. Our linking to such third-party sites does not imply an endorsement or sponsorship of such sites, or the information, products or services offered on or through the sites. In addition, neither we nor affiliates operate or control in any respect any information, products or services that third parties may provide on or through the Website or on websites linked to by us on the Website.
-        </p>
-        <p> If applicable, any opinions, advice, statements, services, offers, or other information or content expressed or made available by third parties, including information providers, are those of the respective authors or distributors, and not GWP. Neither GWP nor any third-party provider of information guarantees the accuracy, completeness, or usefulness of any content. Furthermore, GWP neither endorses nor is responsible for the accuracy and reliability of any opinion, advice, or statement made on any of the Website by anyone other than an authorized GWP representative while acting in his/her official capacity.
-        </p>
-        <p> ANY INFORMATION OFFERED ON OR THROUGH THE WEBSITE AND BY GWP AND ANY THIRD-PARTY SITES ARE PROVIDED “AS IS” AND WITHOUT WARRANTIES OF ANY KIND EITHER EXPRESS OR IMPLIED. IN RELATION TO SUCH INFORMATION, TO THE FULLEST EXTENT PERMISSIBLE PURSUANT TO APPLICABLE LAW, WE DISCLAIM ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. WE DO NOT WARRANT THAT THE WEBSITE OR ANY OF ITS FUNCTIONS WILL BE UNINTERRUPTED OR ERROR-FREE, THAT DEFECTS WILL BE CORRECTED, OR THAT ANY PART OF THIS WEBSITE, ARE FREE OF VIRUSES OR OTHER HARMFUL COMPONENTS. THIS DOES NOT AFFECT YOUR STATUTORY RIGHTS.
-        </p>
-        <p> Please check the Policies section of this Website for details of any specific product warranties that may be offered by any relevant manufacturer of goods sold through this Website.
-        </p>
-        <p> You agree at all times to defend, indemnify and hold harmless GWP its affiliates, their successors, transferees, assignees and licensees and their respective parent and subsidiary companies, agents, associates, officers, directors, shareholders and employees of each from and against any and all claims, causes of action, damages, liabilities, costs and expenses, including legal fees and expenses, arising out of or related to your breach of any obligation, warranty, representation or covenant set forth herein.
-        </p>
-        <p> Compliance All parties are required to comply with UK legislation relating to websites and e-commerce.
-        </p>
-        <p> Unauthorised Use of the Website The following uses are unauthorised, a breach of these conditions and may be an infringement or even a crime: — Any unauthorised use or copying of our name, trading style, get-up, confidential information or data (being information of a confidential nature the misuse of which will foreseeably damage our business reputation or goodwill) or copyright material; — Unauthorised entry to any non-public part of the Website, including its underlying codes, or into any of our private computer system; — Unauthorised copying, downloading or framing of any part of the Website; — Unauthorised covert or overt linking of any part of our Website with another or interception of visitors or potential visitors; and — Any action which is intended or has the effect of deceiving or misleading us or third parties, whether for gain or otherwise.
-        </p>
-        <p> English Law The Website and its contents are governed by English law.
-        </p>
-        <p> Last updated: February 2021
-        </p>
-      </article>
+      <div className="pt-16 px-4 md:px-0">
+
+        <article className="mx-auto prose my-16">
+          <h1>Terms & Conditions</h1>
+          <p>
+            The pages on the website (“the Website”) are published by Greg Williams Photography Limited, registered in
+            England at PO Box 1295, 20 Station Road, Gerrards Cross, Buckinghamshire SL9 8EL with company registration
+            number 04557292 (“GWP”).
+          </p>
+          <p> The Website is intended to serve as an introduction to our company, by giving an outline of the products
+            and services provided through the Website and by providing details of how we can be contacted. The terms and
+            conditions set out below govern the use by you of the Website. We reserve the right to change the terms of
+            use of the Website at any time and you are requested to check the terms of use from time to time to ensure
+            that you have seen the latest version.
+          </p>
+          <p> Copyright and Trademarks Subject to the provisions below, the copyright in all content, graphics, tables,
+            images on the Website belongs to us or our clients and/or licensors and is hereby asserted. Any copying or
+            modification of all or any part of the Website is hereby prohibited with the exception that clients and
+            prospective clients are welcome to take a copy of the pages of the Website, which do not contain materials
+            relating to our clients, as a record of its contents and for personal use only.
+          </p>
+          <p> The Website also contains materials belonging to our clients. These are displayed on the Website with
+            their kind permission, for the purpose of promoting the range of services offered by us and are for display
+            only. Any copying of such materials is strictly prohibited. Please contact us if you require any further
+            clarification in this regard. We hereby assert and reserve all our copyright, design right, moral rights,
+            the moral rights of our employees, community design rights and all other rights in law of whatever type
+            anywhere in the world to protect our intellectual property, goodwill and reputation.
+          </p>
+          <p> If you purchase a print from the Website, please be aware you are purchasing that print only and not any
+            underlying copyright in such print. Please ensure that the prints are not reproduced or published in any
+            other manner or media.
+          </p>
+          <p> Online Commerce You agree to be financially responsible for all purchases made by you or someone acting on
+            your behalf through the Website. You agree to use the Website and to purchase services or products through
+            the Website for legitimate, non-commercial purposes only. You also agree not to make any purchases for
+            speculative, false or fraudulent purposes or for the purpose of anticipating demand for a particular product
+            or service. You agree to only purchase goods or services for yourself or for another person for whom you are
+            legally permitted to do so. When making a purchase for a third party that requires you to submit the third
+            party’s Personally Identifiable Information to us or a merchant, you represent that you have obtained the
+            express consent of such third party to provide such third party’s information.
+          </p>
+          <p> Disclaimers Throughout the Website, we may provide links and pointers to other sites maintained by third
+            parties. Our linking to such third-party sites does not imply an endorsement or sponsorship of such sites,
+            or the information, products or services offered on or through the sites. In addition, neither we nor
+            affiliates operate or control in any respect any information, products or services that third parties may
+            provide on or through the Website or on websites linked to by us on the Website.
+          </p>
+          <p> If applicable, any opinions, advice, statements, services, offers, or other information or content
+            expressed or made available by third parties, including information providers, are those of the respective
+            authors or distributors, and not GWP. Neither GWP nor any third-party provider of information guarantees the
+            accuracy, completeness, or usefulness of any content. Furthermore, GWP neither endorses nor is responsible
+            for the accuracy and reliability of any opinion, advice, or statement made on any of the Website by anyone
+            other than an authorized GWP representative while acting in his/her official capacity.
+          </p>
+          <p> ANY INFORMATION OFFERED ON OR THROUGH THE WEBSITE AND BY GWP AND ANY THIRD-PARTY SITES ARE PROVIDED “AS
+            IS” AND WITHOUT WARRANTIES OF ANY KIND EITHER EXPRESS OR IMPLIED. IN RELATION TO SUCH INFORMATION, TO THE
+            FULLEST EXTENT PERMISSIBLE PURSUANT TO APPLICABLE LAW, WE DISCLAIM ALL WARRANTIES, EXPRESS OR IMPLIED,
+            INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+            WE DO NOT WARRANT THAT THE WEBSITE OR ANY OF ITS FUNCTIONS WILL BE UNINTERRUPTED OR ERROR-FREE, THAT DEFECTS
+            WILL BE CORRECTED, OR THAT ANY PART OF THIS WEBSITE, ARE FREE OF VIRUSES OR OTHER HARMFUL COMPONENTS. THIS
+            DOES NOT AFFECT YOUR STATUTORY RIGHTS.
+          </p>
+          <p> Please check the Policies section of this Website for details of any specific product warranties that may
+            be offered by any relevant manufacturer of goods sold through this Website.
+          </p>
+          <p> You agree at all times to defend, indemnify and hold harmless GWP its affiliates, their successors,
+            transferees, assignees and licensees and their respective parent and subsidiary companies, agents,
+            associates, officers, directors, shareholders and employees of each from and against any and all claims,
+            causes of action, damages, liabilities, costs and expenses, including legal fees and expenses, arising out
+            of or related to your breach of any obligation, warranty, representation or covenant set forth herein.
+          </p>
+          <p> Compliance All parties are required to comply with UK legislation relating to websites and e-commerce.
+          </p>
+          <p> Unauthorised Use of the Website The following uses are unauthorised, a breach of these conditions and may
+            be an infringement or even a crime: — Any unauthorised use or copying of our name, trading style, get-up,
+            confidential information or data (being information of a confidential nature the misuse of which will
+            foreseeably damage our business reputation or goodwill) or copyright material; — Unauthorised entry to any
+            non-public part of the Website, including its underlying codes, or into any of our private computer system;
+            — Unauthorised copying, downloading or framing of any part of the Website; — Unauthorised covert or overt
+            linking of any part of our Website with another or interception of visitors or potential visitors; and — Any
+            action which is intended or has the effect of deceiving or misleading us or third parties, whether for gain
+            or otherwise.
+          </p>
+          <p> English Law The Website and its contents are governed by English law.
+          </p>
+          <p> Last updated: February 2021
+          </p>
+        </article>
+      </div>
       <Footer/>
-    </div>
+    </>
   )
 }
 
