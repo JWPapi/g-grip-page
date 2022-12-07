3f030d3b510a1656b0e1e9219b19ac3ffba2d1e1
JulianWagner <juliantosun@gmail.com>
2022-12-07 10:01:23 +0100

    update

--- components/PricingSection.js
+++ components/PricingSection.js
@@ -1,6 +1,6 @@
 export default function PricingSection({checkoutURL, price, salePrice, freeShipping, onQuantityClick }) {
   return (<section className="mt-16 md:mt-32">
-    <h2 className="text-center mb-4 md:mb-8">Pre-order deal (Get it by 10 Dec)</h2>
+    <h2 className="text-center mb-4 md:mb-8">Pre-order deal (Get it by 15 Dec)</h2>
     <div className="text-8xl leading-none text-center mb-4 -mt-4 tracking-tighter ">
       <del className="line-through decoration-primary block md:inline">{price}</del>
       <span className="text-primary inline-block ml-0 md:ml-6">{salePrice}</span>
@@ -12,7 +12,7 @@ export default function PricingSection({checkoutURL, price, salePrice, freeShipp
         </button>
       </a>
       <div className="mt-2">or <span className="underline cursor-pointer" onClick={() => onQuantityClick(true)}>order multiple</span></div>
-      {freeShipping ? <div className="mt-4">Free shipping • 30 day no risk return</div> :null}
+      {freeShipping ? <div className="mt-4">Free shipping • 30 day no risk return</div> :null} 
     </div>
   </section>)
 }
