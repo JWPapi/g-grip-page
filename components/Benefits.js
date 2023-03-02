c43d7c6955d815dfbe77175aedf2a34d10d89186
JulianWagner <juliantosun@gmail.com>
2023-03-02 12:36:55 +0100

    update

--- components/Benefits.js
+++ components/Benefits.js
@@ -8,21 +8,21 @@ export default function Benefits () {
     <div className="grid grid-cols-2 md:grid-cols-4 md:gap-x-4 gap-y-12 mb-8 ">
       <div className="flex flex-col gap-4">
         <Image src="/icons/benefits/ergonomic-grip.svg" alt="Ergonomic Grip" width={100} height={100}/>
-        <h3 className="text-center">Ergonomic grip</h3>
+     <h5>Ergonomic grip</h5>
       </div>
       <div className="flex flex-col gap-4">
         <Image src="/icons/benefits/wireless-shutter.svg" alt="Bluetooth Shutter" width={100} height={100}/>
-        <h3 className="text-center">Bluetooth® shutter</h3>
+     <h5>Bluetooth® shutter</h5>
 
       </div>
       <div className="flex flex-col gap-4">
         <Image src="/icons/benefits/unique-slide-on- and-off-system.svg" alt="Slide On Off System" width={100}
                height={100}/>
-        <h3 className="text-center">Unique slide on<br />and off system</h3>
+     <h5>Unique slide on<br />and off system</h5>
       </div>
       <div className="flex flex-col gap-4">
         <Image src="/icons/benefits/20-hour-battery.svg" alt="battery life" width={100} height={100}/>
-        <h3 className="text-center">20 hours battery<br />(4,000standby)</h3>
+     <h5>20 hours battery<br />(4,000standby)</h5>
 
       </div>
     </div>
