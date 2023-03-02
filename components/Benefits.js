11cc5dbf836b795dd9cc29c7c3d9afd19b39c759
JulianWagner <juliantosun@gmail.com>
2023-03-02 15:06:49 +0100

    update

--- components/Benefits.js
+++ components/Benefits.js
@@ -6,27 +6,26 @@ export default function Benefits () {
   return (<div className="mb-16">
     <div className="grid grid-cols-2 md:grid-cols-4 md:gap-x-4 gap-y-12 mb-8 ">
       <div className="flex flex-col gap-4 items-center">
-        <div style={{ width: '100px', height: '100px', position: 'relative' }}>
-          <Image src="/icons/benefits/ergonomic-grip.svg" alt="Ergonomic Grip" layout="fill"/>
+        <div style={{ width: '128px', height: '128px', position: 'relative' }}>
+          <Image src="/icons/benefits/grip-icon.svg" alt="Ergonomic Grip" layout="fill"/>
         </div>
         <h5 className="text-center">Ergonomic grip</h5>
       </div>
       <div className="flex flex-col gap-4 items-center">
-        <div style={{ width: '100px', height: '100px', position: 'relative' }}>
-          <Image src="/icons/benefits/wireless-shutter.svg" alt="Bluetooth Shutter" layout="fill"/>
+        <div style={{ width: '128px', height: '128px', position: 'relative' }}>
+          <Image src="/icons/benefits/grip-shutter-icon.svg" alt="Bluetooth Shutter" layout="fill"/>
         </div>
         <h5 className="text-center">Bluetooth® shutter</h5>
       </div>
       <div className="flex flex-col gap-4 items-center">
-        <div style={{ width: '100px', height: '100px', position: 'relative' }}>
-
-          <Image src="/icons/benefits/unique-slide-on- and-off-system.svg" alt="Slide On Off System" layout="fill"/>
+        <div style={{ width: '128px', height: '128px', position: 'relative' }}>
+          <Image src="/icons/benefits/grip-on-off-icon.svg" alt="Slide On Off System" layout="fill"/>
         </div>
         <h5 className="text-center">Unique slide on<br/>and off system</h5>
       </div>
       <div className="flex flex-col gap-4 items-center">
-        <div style={{ width: '100px', height: '100px', position: 'relative' }}>
-          <Image src="/icons/benefits/20-hour-battery.svg" alt="battery life" layout="fill"/>
+        <div style={{ width: '128px', height: '128px', position: 'relative' }}>
+          <Image src="/icons/benefits/grip-battery-icon.svg" alt="battery life" layout="fill"/>
         </div>
         <h5 className="text-center">20 hours battery<br/>(4,000 standby)</h5>
 
