14c1a7eff9a2477c0dc451325667f3126b09add2
JulianWagner <juliantosun@gmail.com>
2023-03-08 12:24:53 -0500

    update

--- components/SlideShow.js
+++ components/SlideShow.js
@@ -1,46 +1,60 @@
-import React from 'react'
-import { Slide } from 'react-slideshow-image'
-import 'react-slideshow-image/dist/styles.css'
+import React, { useState } from "react"
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
+import Image from 'next/image'
 
+export const Slideshow = ()  => {
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const [loaded, setLoaded] = useState(false)
+  const [sliderRef, instanceRef] = useKeenSlider({
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel)
+    },
+    created() {
+      setLoaded(true)
+    },
+  })
 
-
-const divStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  backgroundSize: 'cover',
-  backgroundPosition: 'center',
-  height: '300px',
-}
-
-const slideImages = [
-  {
-    url: '/img/g-grip-fitting-to-iphone.jpg',
-  },
-  {
-    url: '/img/g-grip-on-iphone.jpg',
-  },
-  {
-    url: '/img/g-grip-on-iphone-portrait.jpg',
-  },
-  {
-    url: '/img/g-grip-on-iphone-portrait-2.jpg',
-  },
-  {
-    url: '/img/g-grip-hand-on-iphone.jpg?v=2',
-  },
-]
-
-export const Slideshow = () => {
   return (
-    <div className="slide-container">
-      <Slide indicators="true" arrows={false} transitionDuration={200} autoplay={false}>
-        {slideImages.map((slideImage, index) => (
-          <div key={index}>
-            <div style={{ ...divStyle, backgroundImage: `url(${slideImage.url})` }}></div>
+    <>
+      <div className="navigation-wrapper">
+        <div ref={sliderRef} className="keen-slider">
+          <div className="keen-slider__slide number-slide1">
+            <Image src={'/img/g-grip-hand-on-iphone.jpg?v=2'} alt="ggrip_holding" width={2400} height={1600} />
           </div>
-        ))}
-      </Slide>
-    </div>
+          <div className="keen-slider__slide number-slide2">
+            <Image src={'/img/g-grip-on-iphone.jpg'} alt="ggrip_on_iphone" width={2400} height={1600} />{' '}
+          </div>
+          <div className="keen-slider__slide number-slide3">
+            <Image src={'/img/g-grip-fitting-to-iphone.jpg'} alt="ggrip_attaching" width={2400} height={1600} />{' '}
+          </div>
+          <div className="keen-slider__slide number-slide4">
+            <Image src={'/img/g-grip-on-iphone-portrait.jpg'} alt="ggrip_iphone_portrait" width={2400} height={1600} />{' '}
+          </div>
+          <div className="keen-slider__slide number-slide5">
+            <Image src={'/img/g-grip-on-iphone-portrait-2.jpg'} alt="ggrip_iphone_portrait_2" width={2400} height={1600} />{' '}
+          </div>
+        </div>
+      </div>
+      {loaded && instanceRef.current && (
+        <div className="dots">
+          {[
+            ...Array(instanceRef.current.track.details.slides.length).keys(),
+          ].map((idx) => {
+            return (
+              <button
+                key={idx}
+                onClick={() => {
+                  instanceRef.current?.moveToIdx(idx)
+                }}
+                className={"dot" + (currentSlide === idx ? " active" : "")}
+              ></button>
+            )
+          })}
+        </div>
+      )}
+    </>
   )
 }
+
